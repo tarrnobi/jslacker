312b1f0a53caa63c2b84f9126f675775a90d17ad
Brendan Roberts <tarrnobi@users.noreply.github.com>
2017-08-31 17:42:17 +0100

    Closes #9 , Add leap_years.csv file for testing. More unit tests for new functions

--- README.md
+++ README.md
@@ -3,9 +3,12 @@ JS Implementation of Slacker Unit Tests for SQL Server (Ruby).
 
 
 Intent:
-
-be able to do expect(query("SELECT A=1")).to.equal(jslacker.fromCSV("dummy.csv"))
-
+  Basic Matching:
+    - be able to run a basic parameterized query and compare it to JSON representations of the recordset
+    - be able to run a basic parameterized query and compare it to a CSV
+    - be able to wrap sets of tests in a transaction that is rolled back once completed.
+  - be able to do expect(jslacker.query("SELECT A=1")).to.deep.equal(jslacker.fromCSV("dummy.csv"))
+  - be able to do expect(jslacker.templates.get_people({age: 10}))
 query().. will return a JSON object representing the query result set
 [{a:1}]
 fromCSV will return an array of JSON rows from the CSV
--- test/CSVParser.spec.js
+++ test/CSVParser.spec.js
@@ -1,18 +1,38 @@
-import {expect} from 'chai';
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+
+var expect = chai.expect;
+
 import {CSVParser} from '../lib/jslacker'
 
 
 describe('CSVParser', () =>{
-  it('returns an error message if file does not exist', () =>{
-    return CSVParser.fromCSV("non-existant-file.csv").catch(error => {
-      expect(error).to.equal("Error: File not exists");
+  describe('CSVParser/fromCSV()',() =>{
+    it('returns an error message if file does not exist', () =>{
+      const expectedResults = 'Error: File not exists'
+      expect(CSVParser.fromCSV('non-existant-file.csv')).to.eventually.be.rejectedWith(expectedResults);
+    });
+    it('loads the file given a path', ()=>{
+      const expectedResults = [{Year:'1986', Age:'0'}];
+      expect(CSVParser.fromCSV('dummy.csv')).to.eventually.deep.equal(expectedResults);
     });
   });
-  it('loads the file given a path', ()=>{
-    const expected_data = [{Year:'1986', Age:'0'}];
-    return CSVParser.fromCSV("dummy.csv").then(data =>{
-      expect(data).to.deep.equal(expected_data);
-    })
-  })
-  it('returns json that represents the csv file passed in')
+  describe('CSVParser/prepareAllCSVFiles()', () =>{
+    it('returns an object with a function call for all csv artefacts', () =>{});
+     it('returns json data from the leap_years function when called', ()=>{
+      const expectedResults = [
+        {Year: '1988', Age:'2'},
+        {Year: '1992', Age:'6'},
+        {Year: '1996', Age:'10'},
+        {Year: '2000', Age:'14'},
+        {Year: '2004', Age:'18'},
+        {Year: '2008', Age:'22'},
+        {Year: '2012', Age:'26'},
+        {Year: '2016', Age:'30'}
+      ]
+      const csv = CSVParser.prepareAllCSVFiles();
+      expect(csv.leap_years()).to.eventually.deep.equal(expectedResults);
+    });
+  });
 })
