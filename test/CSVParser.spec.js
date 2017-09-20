3412c780edba1bea099ae8fd2ce0963be5ab0157
Brendan Roberts <tarrnobi@users.noreply.github.com>
2017-09-20 19:55:36 +0100

    Convert tests to async function calls. Should be able to remove chai-as-promised as dependancy

--- test/CSVParser.spec.js
+++ test/CSVParser.spec.js
@@ -9,13 +9,15 @@ const expect = chai.expect;
 
 describe('CSVParser', () => {
   describe('CSVParser/fromCSV()', () => {
-    it('returns an error message if file does not exist', () => {
-      const expectedResults = 'Error: File not exists';
-      return expect(CSVParser.fromCSV('non-existant-file.csv')).to.eventually.be.rejectedWith(expectedResults);
+    it('returns an error message if file does not exist', async () => {
+      const expectedResults = [{ errorMessage: 'Error: File not exists' }];
+      const results = await CSVParser.fromCSV('non-existant-file.csv');
+      expect(results).to.deep.equal(expectedResults);
     });
-    it('loads the file given a path', () => {
+    it('loads the file given a path', async () => {
       const expectedResults = [{ Year: '1986', Age: '0' }];
-      return expect(CSVParser.fromCSV('dummy.csv')).to.eventually.deep.equal(expectedResults);
+      const results = await CSVParser.fromCSV('dummy.csv');
+      expect(results).to.deep.equal(expectedResults);
     });
   });
   describe('CSVParser/prepareAllCSVFiles()', () => {
@@ -23,7 +25,7 @@ describe('CSVParser', () => {
       const csv = CSVParser.prepareAllCSVFiles();
       return expect(csv).to.have.property('leap_years');
     });
-    it('returns json data from the leap_years function when called', () => {
+    it('returns json data from the leap_years function when called', async () => {
       const expectedResults = [
         { Year: '1988', Age: '2' },
         { Year: '1992', Age: '6' },
@@ -35,7 +37,8 @@ describe('CSVParser', () => {
         { Year: '2016', Age: '30' },
       ];
       const csv = CSVParser.prepareAllCSVFiles();
-      return expect(csv.leap_years()).to.eventually.deep.equal(expectedResults);
+      const results = await csv.leap_years();
+      return expect(results).to.deep.equal(expectedResults);
     });
   });
 });
